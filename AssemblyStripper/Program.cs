d585916cd77a037743d7fd6d119260ac4b51b94f
Manta <green.manta@outlook.com>
2020-10-10 07:19:56 -0400

    Initial commit

---
+++ AssemblyStripper/Program.cs
@@ -0,0 +1,99 @@
+﻿using dnlib.DotNet;
+using dnlib.DotNet.Emit;
+using dnlib.DotNet.Writer;
+using System;
+using System.IO;
+
+namespace AssemblyStripper
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Usage: stripper.exe <inputDirectory>");
+                return;
+            }
+
+            string inputDirectory = args[0];
+            if (!Directory.Exists(inputDirectory))
+            {
+                Console.WriteLine($"Couldn't find directory: {inputDirectory}");
+                return;
+            }
+
+            Console.WriteLine($"Stripping assemblies in: {inputDirectory}");
+
+            foreach (string filePath in Directory.GetFiles(inputDirectory))
+            {
+                StripAssembly(filePath);
+            }
+
+            Console.WriteLine("Finished stripping assemblies.");
+        }
+
+        /// <summary>
+        /// Strips the IL from the given assembly, leaving only a reference assembly with no executable code.
+        /// </summary>
+        private static void StripAssembly(string filePath)
+        {
+            Console.WriteLine($"Stripping Assembly: {Path.GetFileName(filePath)}");
+
+            ModuleContext context = ModuleDef.CreateModuleContext();
+            ModuleDefMD module = ModuleDefMD.Load(filePath, context);
+
+            foreach (TypeDef type in module.Types)
+            {
+                StripType(type);
+            }
+
+            // HACK: What if we need to reference embedded resources like localized strings?  Can we empty just the resource CONTENTS?
+            module.Resources?.Clear();
+
+            string outPath = Path.Combine(Path.GetDirectoryName(filePath), "Stripped", Path.GetFileName(filePath));
+            Directory.CreateDirectory(Path.GetDirectoryName(outPath));
+
+            if (module.IsILOnly)
+            {
+                module.Write(outPath, new ModuleWriterOptions(module)
+                {
+                    ShareMethodBodies = true
+                });
+            }
+            else
+            {
+                // Mixed Mode Assembly
+                module.NativeWrite(outPath, new NativeModuleWriterOptions(module, optimizeImageSize: true)
+                {
+                    ShareMethodBodies = true
+                });
+            }
+        }
+
+        /// <summary>
+        /// Strips the given Type's method bodies and nested Types.
+        /// </summary>
+        private static void StripType(TypeDef type)
+        {
+            CilBody throwNullBody = new CilBody
+            {
+                Instructions = { new Instruction(OpCodes.Ldnull), new Instruction(OpCodes.Throw) }
+            };
+
+
+            foreach (MethodDef method in type.Methods)
+            {
+                if (method.HasBody)
+                {
+                    method.Body = throwNullBody;
+                }
+            }
+
+            foreach (TypeDef nestedType in type.NestedTypes)
+            {
+                StripType(nestedType);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+
+# AssemblyStripper
+
+[![Build Status](https://github.com/TheManta/AssemblyStripper/workflows/TODO_WORKFLOW_NAME/badge.svg)](https://github.com/TheManta/AssemblyStripper/actions)
+[![Download Now!](https://img.shields.io/badge/-DOWNLOAD-success)](https://github.com/TheManta/AssemblyStripper/releases/latest)
+
+**AssemblyStripper** is a .NET assembly stripper aimed at generating reference assemblies.  It supports normal and mixed mode assemblies.
+
+## Usage
+
+1. Run `AssemblyStripper <inputDirectory>` in the console.  Every assembly discovered will be stripped into a reference assembly.
+2. Check `<inputDirectory>\Stripped` for the resulting reference assemblies.
